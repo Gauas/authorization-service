aad1d8aeffd435c0c58590f6e6d738ca71c66e67
unknown <quocbao.job106204@gmail.com>
2026-06-02 22:14:55 +0700

    refactor: remove config service dependency

--- config/helper.go
+++ config/helper.go
@@ -5,6 +5,14 @@ import (
 	"strconv"
 )
 
+func getEnv(key, def string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	return v
+}
+
 func getEnvInt(key string, def int) int {
 	v := os.Getenv(key)
 	if v == "" {
