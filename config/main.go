8e9b6c5f46ec7cf6587a22c77e3f66f6a85d5509
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2026-06-03 00:12:24 +0700

    [Fix] Config Biding (#20)

--- config/main.go
+++ config/main.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 
@@ -19,9 +18,6 @@ type Config struct {
 }
 
 func New() Config {
-	if cfg, ok := fromFile(); ok {
-		return cfg
-	}
 	return fromEnv()
 }
 
@@ -35,41 +31,6 @@ func validate(cfg Config) {
 	if cfg.JWTSecretKey == "" {
 		log.Fatal("config: JWT_SECRET_KEY is required")
 	}
-}
-
-const localConfigPath = ".config/config.json"
-
-func fromFile() (Config, bool) {
-	data, err := os.ReadFile(localConfigPath)
-	if err != nil {
-		return Config{}, false
-	}
-
-	var raw map[string]string
-	if err := json.Unmarshal(data, &raw); err != nil {
-		log.Fatalf("config: invalid %s: %v", localConfigPath, err)
-	}
-
-	get := func(key string) string {
-		v := raw[key]
-		if v == "" {
-			log.Fatalf("config: %s is required in %s", key, localConfigPath)
-		}
-		return v
-	}
-
-	cfg := Config{
-		Port:           get("PORT"),
-		SecretKey:      get("SECRET_KEY"),
-		JWTSecretKey:   get("JWT_SECRET_KEY"),
-		JWTExpireSecs:  getEnvInt("JWT_EXPIRE_SECS", 900),
-		RefreshTTLDays: getEnvInt("REFRESH_TTL_DAYS", 30),
-		DBUrl:          get("DB_URL"),
-		CacheURL:       get("CACHE_URL"),
-	}
-
-	validate(cfg)
-	return cfg, true
 }
 
 func fromEnv() Config {
