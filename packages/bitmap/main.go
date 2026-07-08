61d5d8911aafc5f6879bb5e6b6594de7c31b404d
tnqbao <quocbao.job106204@gmail.com>
2026-07-08 22:18:54 +0700

    #refactor: consitency pattern alignment

--- packages/bitmap/main.go
+++ packages/bitmap/main.go
@@ -2,7 +2,7 @@ package bitmap
 
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func Offset[T Integer](id T) (int64, bool) {
